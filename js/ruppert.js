2c2cc048db3fd80fb6c24e71ac22d4f3a939f15d
Eran Geva <eran@eran-mac.local>
2018-08-02 02:49:28 +0300

    impl all, still not work

--- js/ruppert.js
+++ js/ruppert.js
@@ -1,17 +1,77 @@
 $(document).ready(function () {
+  var img = document.getElementById('img');
+  var canvas2 = document.createElement('canvas');
+  canvas2.width = img.width;
+  canvas2.height = img.height;
+  canvas2.getContext('2d').drawImage(img, 0, 0, img.width, img.height);
+  var pixelData = canvas2.getContext('2d').getImageData(0, 0, img.width, img.height).data;
+  // debugger
+
+  function channel(pos, c) {
+    return pixelData[(pos[0] * img.width + pos[1]) * 4 + c];
+  }
+
+  function pixheight(pos) {
+    return channel(pos, 0) + channel(pos, 1) + channel(pos, 2);
+  }
+
+  function cross3(a, b) {
+    var ax = a[0], ay = a[1], az = a[2];
+    var bx = b[0], by = b[1], bz = b[2];
+    var x = ay * bz - az * by;
+    var y = az * bx - ax * bz;
+    var z = ax * by - ay * bx;
+    return [x, y, z];
+  }
+
+  function sub3(a, b) {
+    return [a[0] - b[0], a[1] - b[1], a[2] - b[2]];
+  }
+
+  function norm3(v) {
+    var d = Math.sqrt(v[0] * v[0] + v[1] * v[1] + v[2] * v[2]);
+    d = d || 1;
+    return [v[0] / d, v[1] / d, v[2] / d];
+  }
+
+  function dot3(u, v) {
+    return u[0] * v[0] + u[1] * v[1] + u[2] * v[2];
+  }
+
+  function isBadOnImage(a, b, c) {
+    var center = [(a[0] + b[0] + c[0]) / 3.0, (a[1] + b[1] + c[1]) / 3.0];
+    var ah = pixheight(a), bh = pixheight(b), ch = pixheight(c);
+    var cenh = pixheight(center);
+
+    var pn = norm3(cross3(sub3(c, b), sub3(a, b)));
+    var pd = -dot3(a, pn);
+
+    var linestart = [center[0], center[1], 0];
+    var direction = [0,0,1];//line.delta( v1 );
+    var denominator = pn[2];//this.normal.dot( direction );
+    var imh = - ( dot3(linestart, pn) + pd ) / denominator;
+    return Math.abs(imh - cenh) > 1;
+  }
 
   var canvas = $('#canvas');
-  /*
-  var vertices = [[100, 200],[100, 300],[300, 400],[300, 300],[400, 200],[300, 200],[300, 100],[200, 200],[200, 100],[150, 200],[250, 300],[150, 300]];
+
+  var vertices = [[0, 0],[0, img.height],[img.width, img.height],[img.width, 0]];
   Graph.fitVerticesInto(vertices, canvas.width(), canvas.height());
-  var edges = [[ 0,  1],[ 1,  2],[ 2,  3],[ 3,  4],[ 4,  5],[ 5,  6],[ 6,  7],[ 7,  8],[ 8,  0],[ 9, 10],[10, 11],[11,  9]];
+  var edges = [[0, 2]];
   Graph.markFixed(edges);
   Graph.markExternal(edges);
-  var face = [[0, 1, 2, 3, 4, 5, 6, 7, 8],[9,10,11]];
-  */
-  var vertices = Graph.fitVerticesInto(key.vertices, canvas.width(), canvas.height());
-  var edges = key.edges.slice();
-  var face = key.faces[0];
+  var face = [[0, 1, 2, 3]];
+
+  // var vertices = [[100, 200],[100, 300],[300, 400],[300, 300],[400, 200],[300, 200],[300, 100],[200, 200],[200, 100],[150, 200],[250, 300],[150, 300]];
+  // Graph.fitVerticesInto(vertices, canvas.width(), canvas.height());
+  // var edges = [[ 0,  1],[ 1,  2],[ 2,  3],[ 3,  4],[ 4,  5],[ 5,  6],[ 6,  7],[ 7,  8],[ 8,  0],[ 9, 10],[10, 11],[11,  9]];
+  // Graph.markFixed(edges);
+  // Graph.markExternal(edges);
+  // var face = [[0, 1, 2, 3, 4, 5, 6, 7, 8],[9,10,11]];
+
+  // var vertices = Graph.fitVerticesInto(key.vertices, canvas.width(), canvas.height());
+  // var edges = key.edges.slice();
+  // var face = key.faces[0];
   var diags = triangulate.face(vertices, face);
   edges = edges.concat(diags);
   var qe = triangulate.makeQuadEdge(vertices, edges);
@@ -28,7 +88,8 @@ $(document).ready(function () {
   triangulate.refineToRuppert(vertices, edges, qe.coEdges, qe.sideEdges, {
     minAngle: 30,
     maxSteinerPoints: 300,
-    trace: trace
+    trace: trace,
+    isBad: isBadOnImage
   });
 
   g = new Graph(vertices, edges, [face]);
