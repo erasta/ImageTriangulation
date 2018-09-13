b427846f20c35103fbbf204213026fa18c3826cd
Eran Geva <>
2018-09-13 12:15:57 +0300

    fixSizesByImage()

--- js/loadimage.js
+++ js/loadimage.js
@@ -23,5 +23,13 @@ function loadImage(img) {
     canvas.height = img.height;
     canvas.getContext('2d').drawImage(img, 0, 0, img.width, img.height);
     var imageData = canvas.getContext('2d').getImageData(0, 0, img.width, img.height);
+    fixSizesByImage();
     return imageData;
+}
+
+function fixSizesByImage() {
+    setTimeout(function() {
+        $('#canvas')[0].width = $('#chosenWidth')[0].value = $('#widthImage')[0].value = $('#img')[0].width;
+        $('#canvas')[0].height = $('#chosenHeight')[0].value = $('#img')[0].height;
+    }, 10);
 }
