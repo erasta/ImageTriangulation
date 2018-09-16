c9efe9e6434111fe8bd91fd4b9298674fb1acc6a
Eran Geva <>
2018-09-16 14:52:41 +0300

    download image

--- js/loadimage.js
+++ js/loadimage.js
@@ -10,20 +10,20 @@ function readImageAsUrl(f) {
 }
 
 $("document").ready(function () {
-    $("#loadImage").change(function (e) {readImageAsUrl(e.target.files[0]);});
+    $("#loadImage").change(function (e) { readImageAsUrl(e.target.files[0]); });
 
-    $(document).on('input', '#Iterations', function (e) {$('#chosenIterations')[0].value = $('#Iterations')[0].value;});
-    $(document).on('input', '#Sensitivity', function (e) {$('#chosenSensitivity')[0].value = $('#Sensitivity')[0].value;});
+    $(document).on('input', '#Iterations', function (e) { $('#chosenIterations')[0].value = $('#Iterations')[0].value; });
+    $(document).on('input', '#Sensitivity', function (e) { $('#chosenSensitivity')[0].value = $('#Sensitivity')[0].value; });
 
     $(document).on('input', '#widthImage', function (e) {
         $('#img')[0].width = $('#canvas')[0].width = $('#chosenWidth')[0].value = $('#widthImage')[0].value;
-        setTimeout(function() {
+        setTimeout(function () {
             $('#canvas')[0].height = $('#chosenHeight')[0].value = $('#img')[0].height;
         }, 10);
     });
 
-    $('#img').on('dragover', function (e) {e.stopPropagation();    e.preventDefault();});
-    $('#img').on('drop', function(e) {
+    $('#img').on('dragover', function (e) { e.stopPropagation(); e.preventDefault(); });
+    $('#img').on('drop', function (e) {
         e.preventDefault();
         readImageAsUrl(e.originalEvent.dataTransfer.files[0]);
     });
@@ -44,7 +44,7 @@ function loadImage(img) {
 }
 
 function fixSizesByImage() {
-    setTimeout(function() {
+    setTimeout(function () {
         $('#canvas')[0].width = $('#chosenWidth')[0].value = $('#widthImage')[0].value = $('#img')[0].width;
         $('#canvas')[0].height = $('#chosenHeight')[0].value = $('#img')[0].height;
     }, 10);
@@ -56,3 +56,11 @@ function fixSizesByImage() {
 //     var data = ev.dataTransfer.getData("text");
 //     // ev.target.appendChild(document.getElementById(data));
 // }
+
+function downloadImage() {
+    var download = document.getElementById("download");
+    var image = document.getElementById("canvas").toDataURL("image/png")
+        .replace("image/png", "image/octet-stream");
+    download.setAttribute("href", image);
+    //download.setAttribute("download","archive.png");
+}
